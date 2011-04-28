19568f63c7eea13682decbea724fc1d8c297c607
Chuck Ha <cha@mbl.edu>
2011-04-28 12:01:25 -0400

    updated the file logic, uploading a file works

--- hpshelper.rb
+++ hpshelper.rb
@@ -1,3 +1,4 @@
+require "fileutils"
 require "bundler/setup"
 
 require "sinatra"
@@ -8,10 +9,7 @@ get '/' do
 end
 
 post '/upload' do
-    unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-        erb :upload
-    end
-    while blk = tmpfile.read(65536)
-        File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
-    end
+    puts params.inspect
+    params["file"][:tempfile].read
 end
+
