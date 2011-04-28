73a62f8062595224e6e438574fa008b8ed9c2d48
Chuck Ha <cha@mbl.edu>
2011-04-28 11:10:09 -0400

    added basic HTML structure and some uploading logic

--- hpshelper.rb
+++ hpshelper.rb
@@ -6,3 +6,12 @@ require "erb"
 get '/' do
     erb :index
 end
+
+post '/upload' do
+    unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+        erb :upload
+    end
+    while blk = tmpfile.read(65536)
+        File.open(File.join(Dir.pwd,"public/uploads", name), "wb") { |f| f.write(tmpfile.read) }
+    end
+end
