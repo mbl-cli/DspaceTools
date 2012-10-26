303217c68f2a7f70c6e7e363aca94b55f8d333f4
Dmitry Mozzherin <dmozzherin@gmail.com>
2012-10-26 14:29:40 -0400

    added dspace command

--- lib/dspace_csv.rb
+++ lib/dspace_csv.rb
@@ -5,17 +5,13 @@ module DSpaceCSV
     
   def self.submit(path, collection_id, user)
     remote_path = File.join(DSpaceCSV::Conf.remote_tmp_dir, 'csv_' + path.match(/(dspace_[\d]+)/)[1])
-    `ssh #{DSpaceCSV::Conf.remote_login} 'mkdir -p #{remote_path}'`
-    Dir.entries(path).each do |e|
-      next unless e.match(/[\d]{4}/)
-      dir_path = File.join(path, e)
-      remote_dir_path = File.join(remote_path, e)
-      `scp -r #{dir_path} #{DSpaceCSV::Conf.remote_login}:#{remote_path}`
-      params = [DSpaceCSV::Conf.dspace_path, user["email"], collection_id, remote_dir_path, File.join(remote_dir_path, 'dublin_core.xml') ]
-      dspace_command = "%s import ItemImport -a -e %s -c %s -s %s -m %s" % params
-      # /dspace/bin/dspace import ItemImport -a -e drielinger@mbl.edu -c 28 -s /tmp/csv_dspace_7773576859/ -m /tmp10242012BMI
-      params[0] ? `ssh #{DSpaceCSV::Conf.remote_login} '#{dspace_command}'` : puts(dspace_command)
-    end
+    `scp -r #{path} #{DSpaceCSV::Conf.remote_login}:#{remote_path}`
+    map_file = Time.now().to_s[0..9].gsub('-','_') + '_mapfile_' + user["name"].downcase.gsub(' ', '_')
+    params = [DSpaceCSV::Conf.dspace_path, user["email"], collection_id, remote_path, File.join(remote_dir_path, map_file)]
+    dspace_command = "%s import ItemImport -a -e %s -c %s -s %s -m %s" % params
+    params[0] ? `ssh #{DSpaceCSV::Conf.remote_login} '#{dspace_command}'` : puts(dspace_command)
+    `scp #{DSpaceCSV::Conf.remote_login}:#{File.join(DSpaceCSV::Conf.remote_tmp_dir, map_file)} #{DSpaceCSV::Conf.tmp_dir}`
+    File.join(DSpaceCSV::Conf.tmp_dir, map_file)
   end
 
 end
