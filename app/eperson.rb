ce9211ed1dda44c7c1c35d4837e949c7f8cf0c11
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-01-18 13:43:12 -0500

    removed outdated files

--- app/eperson.rb
+++
@@ -1,6 +0,0 @@
-class Eperson < ActiveRecord::Base
-  self.table_name = 'eperson'
-  self.primary_key = 'eperson_id'
-  has_many :eperson_groups, :class_name => 'EpersonGroup'
-  has_many :groups, :through => :eperson_groups
-end
--- app/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ActiveRecord::Base
-  self.table_name = 'epersongroup'
-  self.primary_key = 'eperson_group_id'
-  has_many :eperson_groups, :class_name => 'EpersonGroup'
-  has_many :epersons, :through => :eperson_groups
-end
